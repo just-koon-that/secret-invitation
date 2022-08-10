900bb8c3d240eea1e9131d65b5134fb70f8d23a0
thomas-ktx <thomas.ktx@kakaocorp.com>
2022-08-10 18:52:43 +0900

    feat: 메인 컴포넌트 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
+import MainBanner from './components/MainBanner';
 
 function App() {
   return (
-    <main>
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
+    <main className="bg-gray-200">
+      <div className="max-w-md m-auto  bg-white">
+        <MainBanner />
+        <h1 className="text-3xl font-bold underline">
+          Hello world!
+        </h1>
+      </div>
     </main>
   );
 }
